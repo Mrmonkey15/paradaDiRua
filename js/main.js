9fccdb0f5308a3f0d1f26ea4196c865dd29385fc
Mrmonkey15 <lucas.network15@gmail.com>
2024-08-05 17:41:08 -0300

    up

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
-//Sidebar
-
-document.addEventListener('DOMContentLoaded', function() {
-    let burgerMenu = document.querySelector('.burger-menu');
-    let closeSidebarButton = document.querySelector('.close-sidebar');
-    let sidebar = document.querySelector('.sidebar');
-
-    burgerMenu.addEventListener('click', function() {
-        burgerMenu.classList.toggle('open');
-        sidebar.classList.toggle('open');
-    });
-
-    closeSidebarButton.addEventListener('click', function() {
-        burgerMenu.classList.remove('open');
-        sidebar.classList.remove('open');
-    });
-});
+// Side-bar//
+document.addEventListener('DOMContentLoaded', function(){
+    let burgerMenu = document.getElementById(burgerMenuIcon)
+    
+})
