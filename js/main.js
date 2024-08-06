01a744acb886cbe6b4f9b0055ee762b9402487a1
Mrmonkey15 <lucas.network15@gmail.com>
2024-08-06 17:09:45 -0300

    updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 // Side-bar//
 document.addEventListener('DOMContentLoaded', function(){
     let burgerMenu = document.getElementById('burgerMenuIcon')
-    
+    let closebutton = document.getElementById('closebtn')
+    let sidebarDiv = document.querySelector('.sidebar')
+
+burgerMenu.addEventListener('click', function(){
+    sidebarDiv.classList.toggle('open');
+})
+
+closebutton.addEventListener('click', function(){
+    sidebarDiv.classList.remove('open')
+})
 })
